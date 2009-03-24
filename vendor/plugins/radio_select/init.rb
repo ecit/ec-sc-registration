2572775ac34422489aacc6d8493c91fa3ef6aba8
Hans-Thomas Mueller <htmue@gmx.net>
2009-03-24 17:22:32 +0100

    Created radio_select plugin.

---
+++ vendor/plugins/radio_select/init.rb
@@ -0,0 +1,1 @@
+require 'radio_select'
---
+++ vendor/plugins/radio_select/lib/radio_select.rb
@@ -0,0 +1,29 @@
+module ActionView
+  module Helpers
+
+    module FormOptionsHelper
+      def radio_select(object, method, choices, options = {}, html_options = {})
+        InstanceTag.new(object, method, self, options.delete(:object)).to_radio_select_tag(choices, options, html_options)
+      end
+    end
+
+    class InstanceTag
+      def to_radio_select_tag(choices, options, html_options)
+        html_options = html_options.stringify_keys
+        add_default_name_and_id(html_options)
+        name = html_options.delete 'name'
+        value = value(object)
+        choices.map do |choice|
+          label_tag "#{name}_#{choice[1]}", "#{radio_button_tag name, choice[1], choice[1] == value}#{choice[0]}"
+        end.join " "
+      end
+    end
+    
+    class FormBuilder
+      def radio_select(method, choices, options = {}, html_options = {})
+        @template.radio_select(@object_name, method, choices, options.merge(:object => @object), html_options)
+      end
+    end
+
+  end
+end
